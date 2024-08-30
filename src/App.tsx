8262a1d988d5be105b7e41155b431e334dcdf237
j-tee <juliustetteh@gmail.com>
2024-08-30 10:32:11 +0000

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -27,35 +27,35 @@ function App() {
   }
   const [quote, setQuote] = useState(getQuote())
   const [color, setColor] = useState<string>(getColor())
-
+  const transition = "all 1.5s";
   const changeQuote = () => {
     setQuote(getQuote())
     setColor(getColor())
   }
 
   return (
-    <div className="background" style={{ backgroundColor: color }}>
+    <div className="background" style={{ backgroundColor: color, transition }}>
       <div id="wrapper">
         <div id="quote-box">
 
-          <div className="quote-text" style={{ color: color }} id="text">
+          <div className="quote-text" style={{ color: color, transition }} id="text">
             <h4><FaQuoteLeft size={30} style={{marginRight:"10px"}} />
               {quote.quote}
               <FaQuoteRight style={{marginLeft: "10px"}} size={30} />
             </h4></div>
 
-          <div className="quote-author" style={{ color: color }} id="author">~ {quote.author}</div>
+          <div className="quote-author" style={{ color: color, transition }} id="author">~ {quote.author}</div>
           <div className="buttons">
             <span>
-              <a style={{ backgroundColor: color }} id="tweet-quote" href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&{quote.quote}" className="btn btn-primary">
+              <a style={{ backgroundColor: color, transition }} id="tweet-quote" href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&{quote.quote}" className="btn btn-primary">
                 <FontAwesomeIcon icon={faTwitter} size="1x" />
               </a>
-              <a style={{ backgroundColor: color }} href="https://tumblr.com" className="btn btn-primary">
+              <a style={{ backgroundColor: color, transition }} href="https://tumblr.com" className="btn btn-primary">
                 <FontAwesomeIcon icon={faTumblr} size="1x" />
               </a>
             </span>
             <span>
-              <Button style={{ backgroundColor: color }} id="new-quote" variant="primary" onClick={changeQuote}>
+              <Button style={{ backgroundColor: color, transition }} id="new-quote" variant="primary" onClick={changeQuote}>
                 New Quote
               </Button>
             </span>
